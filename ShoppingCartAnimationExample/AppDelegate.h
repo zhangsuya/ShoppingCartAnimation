59627626032675e4b22d167a4c76971c8792ef96
苏亚 <792708835@qq.com>
2016-03-23 10:18:42 +0800

    新建项目喽

---
+++ ShoppingCartAnimationExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingCartAnimationExample
+//
+//  Created by Mac on 16/3/23.
+//  Copyright © 2016年 suya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingCartAnimationExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShoppingCartAnimationExample
+//
+//  Created by Mac on 16/3/23.
+//  Copyright © 2016年 suya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
